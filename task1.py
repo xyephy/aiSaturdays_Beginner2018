7e8b63f5f4abb4fd6c29a3726122924914003d24
xyephy <ziphyr.xyeag@gmail.com>
2018-08-01 13:09:47 +0300

    Final codes for Task1 and Task2

--- task1.py
+++ task1.py
@@ -1,9 +1,14 @@
-import random 
+import random
 
 ourList = list()
-count = 0 
+num = 5
+count = 0
 while (count < 11):
-    ourList.append(random.randint(1,10))
+    ourList.append(random.randint(1,10)) # generates ten random numbers
     count += 1
-    
-ourList
+
+# we assign belowFive numbers in ourList below 5   
+belowFive = [i for i in ourList if i < num]
+
+#print numbers below five
+print (belowFive)
--- task2.py
+++ task2.py
@@ -3,4 +3,13 @@ import random
 words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
-randomPhrase
+#reversing the words 
+reversePhrase = randomPhrase[::-1]
+
+# output for random words
+print("Words in normal Display: ")
+print(randomPhrase)
+print("-" * 30)
+# output for reverse of random words
+print("Words in reverse Display: ")
+print (reversePhrase)
